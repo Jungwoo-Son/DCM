56b06c5868ac81fab6edea0cbd608f96e7a25346
Jungwoo-Son <jungwoo9356@gmail.com>
2021-03-12 17:13:25 +0900

    [ADD] 프로젝트 구조 생성

---
+++ Backend/src/configs/index.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+module.exports = {
+    PORT: process.env.PORT,
+    DB_DATABASE: process.env.DB_DATABASE,
+    DB_USERNAME: process.env.DB_USERNAME,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    TOKEN_SECRET: process.env.TOKEN_SECRET,
+    DB_HOST: process.env.DB_HOST,
+};
