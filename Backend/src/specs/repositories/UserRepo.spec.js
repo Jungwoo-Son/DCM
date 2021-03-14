79776f897fdad038d0aa53833a0eb522d275a81c
Jungwoo-Son <jungwoo9356@gmail.com>
2021-03-15 07:52:11 +0900

    [ADD] UserRepo 테스트 작성

---
+++ Backend/src/specs/repositories/UserRepo.spec.js
@@ -0,0 +1,29 @@
+const { UserRepo } = require('../../repositories');
+const { sequelize } = require('../../loaders/database');
+const { UserBuilder } = require('../../models/User');
+
+let simple_db_status = [new UserBuilder('홍길동', '010-1234-5678').setId('asdf').setPw('qwer').build()]
+
+describe('spec of UserRepo', () => {
+    let transaction;
+    beforeEach(async () => {
+        transaction = await sequelize.transaction();        
+    });
+    afterEach(async () => {
+        await transaction.rollback();
+    });
+    it('should find all users from db', async () => {
+        const users = await UserRepo.findAllUsers(transaction);
+        expect(users).toEqual(simple_db_status);
+    });
+    it('should find a user by id', async () => {
+        const user = await UserRepo.findById('asdf', transaction);
+        expect(user).toStrictEqual(simple_db_status[0]);
+    });
+    it('should insert a user into db', async () => {
+        const new_user = new UserBuilder('김철수', 'kemail@gmail.com').setId('chsl').setPw('tuyi').build();
+        await UserRepo.create(new_user, transaction);
+        const users = await UserRepo.findAllUsers(transaction);
+        expect(users).toContainEqual(new_user);
+    });
+});
