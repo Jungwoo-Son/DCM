e816f88f4026cfac35bfc87d3ed2c43e59f1c7a3
Jungwoo-Son <jungwoo9356@gmail.com>
2021-03-18 11:23:43 +0900

    [ADD] UserService

---
+++ Backend/src/services/UserService.js
@@ -0,0 +1,21 @@
+const { UserBuilder } = require('../models/User');
+const { UserRepo } = require('../repositories');
+
+
+class UserService {
+    constructor() {
+
+    }
+    
+    static async createNewUser(id, pw, name, contact) {
+        const new_user = new UserBuilder(id, name, contact).setPw(pw).build();
+        await UserRepo.create(new_user);
+    }
+
+    static async getUserById(id) {
+        const user = await UserRepo.findById(id);
+        return user;
+    }
+}
+
+module.exports = UserService;
