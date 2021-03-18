7b0dcfbdd374a1867bd1434be4156761bb080f8e
Jungwoo-Son <jungwoo9356@gmail.com>
2021-03-18 21:44:59 +0900

    [UPDATE] ProjectRepo Mocking, create, findAllProjects, findById

--- Backend/src/repositories/definitions/__mocks__/ProjectRepo.js
+++ Backend/src/repositories/definitions/__mocks__/ProjectRepo.js
@@ -1,9 +1,30 @@
 const { ProjectBuilder } = require('../../../models/Project');
 
-let mocking_user_datas;
-const mock = jest.fn().mockImplementation(() => {
-    simple_user_datas = [ new ProjectBuilder('프로젝트1').setId(10).build() ];
+let mocking_project_datas;
+const mock = {};
+mock.mockClear = jest.fn().mockImplementation(() => {
+    mocking_project_datas = [ new ProjectBuilder('프로젝트1').setId(10).build() ];
+    mock.create.mockClear();
+    mock.findAllProjects.mockClear();
+    mock.findById.mockClear();
 });
+
+mock.create = jest
+    .fn()
+    .mockImplementation((project) => {
+        mocking_project_datas.push(project);
+    });
+mock.findAllProjects = jest
+    .fn()
+    .mockImplementation(() => {
+        return mocking_project_datas;
+    });
+mock.findById = jest
+    .fn()
+    .mockImplementation((id) => {
+        const project = mocking_project_datas.filter((project) => project.getId() == id);
+        return project[0];
+    });
 
 mock.hasMany = jest.fn();
 mock.belongsTo = jest.fn();
