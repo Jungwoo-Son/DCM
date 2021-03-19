d233bd484c5ca5669182cb32bc52d258f1d31c4e
Jungwoo-Son <jungwoo9356@gmail.com>
2021-03-19 10:17:07 +0900

    [ADD] UserController 테스트 작성

---
+++ Backend/src/specs/controllers/UserController.spec.js
@@ -0,0 +1,28 @@
+jest.mock('../../repositories/definitions/UserRepo');
+jest.mock('../../repositories/definitions/ProjectRepo');
+jest.mock('../../repositories/definitions/ComponentRepo');
+const { UserRepo, ProjectRepo, ComponentRepo} = require('../../repositories');
+const UserControllers = require('../../apis/routes/UserController');
+
+const RequestBuilder = require('./FakeRequest');
+const Response = require('./FakeResponse');
+
+
+
+describe('spec of UserController', () => {
+    beforeEach(() => {
+        UserRepo.mockClear();
+        ProjectRepo.mockClear();
+        ComponentRepo.mockClear();
+    });
+    it('should get user', async () => {
+        const req = new RequestBuilder().setParams({id: 'asdf'}).build();
+        const res = new Response;
+        await UserControllers.getUserById(req, res);
+        expect(res.result).toEqual({
+            id: 'asdf',
+            name: '홍길동',
+            contact: '010-1234-5678'
+        });
+    });
+});
