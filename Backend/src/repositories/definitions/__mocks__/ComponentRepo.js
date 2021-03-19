47c72a97cae24be7395b9347cb1facbe5864bb9a
Jungwoo-Son <jungwoo9356@gmail.com>
2021-03-19 09:24:24 +0900

    [UPDATE] ComponentRepo Mocking 구현

--- Backend/src/repositories/definitions/__mocks__/ComponentRepo.js
+++ Backend/src/repositories/definitions/__mocks__/ComponentRepo.js
@@ -1,9 +1,29 @@
 const { ComponentBuilder } = require('../../../models/Component');
 
-let mocking_user_datas;
-const mock = jest.fn().mockImplementation(() => {
-    simple_user_datas = [ new ComponentBuilder('요소1', 'asdf', 10).setId(10).build() ];
+let mocking_component_datas;
+const mock = {};
+mock.mockClear = jest.fn().mockImplementation(() => {
+    mocking_component_datas = [ new ComponentBuilder('요소1', 'asdf', 10).setId(10).build() ];
+
 });
+
+mock.findByProjectId = jest.fn()
+    .mockImplementation((project_id) => {
+        return mocking_component_datas.filter((component) => component.getProjectId() === project_id);
+    });
+mock.findByUserId = jest.fn()
+    .mockImplementation((user_id) => {
+        return mocking_component_datas.filter((component) => component.getManagerId() === user_id);
+    });
+mock.findById = jest.fn()
+    .mockImplementation((id) => {
+        return mocking_component_datas.filter((component) => component.getId() === id);
+    });
+mock.create = jest.fn()
+    .mockImplementation((component) => {
+        const new_component = new ComponentBuilder(component.getName(), component.getManagerId(), component.getProjectId()).setId(11).build();
+        mocking_component_datas.push(new_component);
+    });
 
 mock.hasMany = jest.fn();
 mock.belongsTo = jest.fn();
