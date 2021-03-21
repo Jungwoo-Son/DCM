3ced0bda2dc65d1db5dced3ef367cd4938c35286
Jungwoo-Son <jungwoo9356@gmail.com>
2021-03-21 21:04:45 +0900

    [ADD] component controller 작성

---
+++ Backend/src/apis/routes/ComponentController.js
@@ -0,0 +1,21 @@
+const { ComponentBuilder } = require('../../models/Component');
+const ComponentService = require('../../services/ComponentService');
+
+const ComponentContollers = {};
+
+ComponentContollers.getAllComponentOfTheProject = async (req, res, next) => {
+    const project_id = req.params.id;
+
+    const components = await ComponentService.getComponentsOfTheProject(project_id);
+    res.send(components);
+};
+ComponentContollers.createComponent = async (req, res, next) => {
+    const project_id = req.params.id;
+    const name = req.body.name;
+    const manager_id = req.body.manager;
+    const component = new ComponentBuilder(name, manager_id, project_id).build();
+    await ComponentService.createNewComponent(component);
+    res.send();
+};
+
+module.exports = ComponentContollers;
