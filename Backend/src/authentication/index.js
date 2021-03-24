1c4a187bf88da21014b4a88da54c56043524dbff
Jungwoo-Son <jungwoo9356@gmail.com>
2021-03-24 15:58:26 +0900

    [FIX] 이름 변경 Authorization -> Authentication, fromStringed~~Token -> fromString

--- Backend/src/authentication/index.js
+++ Backend/src/authentication/index.js
@@ -15,7 +15,7 @@ class AccessToken {
             });
         }
     }
-    static fromStringedAccessToken(token) {
+    static fromString(token) {
         const payload = JWT.verify(token, TOKEN_SECRET, { subject: 'access_token' });
         const access_token = new AccessToken();
         access_token.token = token;
@@ -30,7 +30,7 @@ class AccessToken {
     }
 }
 
-class RefresToken {
+class RefreshToken {
     constructor(user_id) {
         if (user_id) {
             this.user_id = user_id;
@@ -45,9 +45,9 @@ class RefresToken {
         }
     }
 
-    static fromStringedRefreshToken(token) {
+    static fromString(token) {
         const payload = JWT.verify(token, TOKEN_SECRET, { subject: 'refresh_token' });
-        const refresh_token = new RefresToken();
+        const refresh_token = new RefreshToken();
         refresh_token.token = token;
         refresh_token.user_id = payload.user_id;
         return refresh_token;
@@ -61,5 +61,5 @@ class RefresToken {
 }
 module.exports = {
     AccessToken,
-    RefresToken,
+    RefreshToken,
 };
