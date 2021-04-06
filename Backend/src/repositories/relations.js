c0bf9ec39c2b68e5091feae24f572d92f270d066
Jungwoo-Son <jungwoo9356@gmail.com>
2021-04-06 16:54:04 +0900

    [ADD] User와 project간 관계 설정

--- Backend/src/repositories/relations.js
+++ Backend/src/repositories/relations.js
@@ -1,12 +1,16 @@
 const UserRepo = require('./definitions/UserRepo');
 const ProjectRepo = require('./definitions/ProjectRepo');
 const ComponentRepo = require('./definitions/ComponentRepo');
+const UserProjectRepo = require('./definitions/UserProjectRepo');
 
 UserRepo.hasMany(ComponentRepo, {foreignKey: 'manager', sourceKey: 'id'});
 ComponentRepo.belongsTo(UserRepo, {foreignKey: 'manager', sourceKey: 'id'});
 
 ProjectRepo.hasMany(ComponentRepo, {foreignKey: 'project_id', sourceKey: 'id'});
 ComponentRepo.belongsTo(ProjectRepo, {foreignKey: 'project_id', sourceKey: 'id'});
+
+UserRepo.belongsToMany(ProjectRepo, {through: UserProjectRepo, as: 'projects', foreignKey: 'user_id', otherKey: 'project_id'});
+ProjectRepo.belongsToMany(UserRepo, {through: UserProjectRepo, as: 'users', foreignKey: 'project_id', otherKey: 'user_id'});
 
 module.exports = {
     UserRepo,
