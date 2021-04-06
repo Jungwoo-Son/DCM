e20429315497394849690073f52bba768e66b62d
Jungwoo-Son <jungwoo9356@gmail.com>
2021-04-06 16:53:12 +0900

    [ADD] findByIdWithProjects 테스트 작성

--- Backend/src/specs/repositories/UserRepo.spec.js
+++ Backend/src/specs/repositories/UserRepo.spec.js
@@ -1,6 +1,7 @@
 const { UserRepo } = require('../../repositories');
 const { sequelize } = require('../../loaders/database');
 const { UserBuilder } = require('../../models/User');
+const { ProjectBuilder } = require('../../models/Project');
 
 let simple_db_status = [new UserBuilder('asdf', '홍길동', '010-1234-5678').setPw('qwer').build()]
 
@@ -28,5 +29,11 @@ describe('spec of UserRepo', () => {
         await UserRepo.create(new_user, transaction);
         const users = await UserRepo.findAllUsers(transaction);
         expect(users).toContainEqual(new_user);
+    });
+    it('should find a user by id with projects', async () => {
+        const user = await UserRepo.findByIdWithProjects('asdf', transaction);
+        const expected_user_with_projects = new UserBuilder('asdf', '홍길동', '010-1234-5678').setPw('qwer').build();
+        expected_user_with_projects.addProject(new ProjectBuilder('프로젝트1').setId(10).build());
+        expect(user).toEqual(expected_user_with_projects);
     });
 });
