c4ba3e8677dab5f3b951445151d1be83bdc850fe
Jungwoo-Son <jungwoo9356@gmail.com>
2021-04-06 16:55:20 +0900

    [ADD] findByIdWithProjects'

--- Backend/src/repositories/definitions/UserRepo.js
+++ Backend/src/repositories/definitions/UserRepo.js
@@ -1,6 +1,8 @@
 const { sequelize } = require('../../loaders/database');
 const { Model, DataTypes } = require("sequelize");
 const { UserBuilder } = require('../../models/User');
+const { ProjectBuilder } = require('../../models/Project');
+const ProjectRepo = require('./ProjectRepo');
 
 class UserRepo extends Model {
     static async findAllUsers(transaction) {
@@ -25,10 +27,29 @@ class UserRepo extends Model {
             transaction,
         });
     }
+    static async findByIdWithProjects(id, transaction) {
+        const sequelize_user = await super.findByPk(id, {
+            include: { model: ProjectRepo, as: 'projects' },
+            transaction
+        });
+
+        const projects = sequelize_user.projects.map((sequelize_project) => convertSequelizeProjectToModel(sequelize_project));
+        const user = convertSequelizeModelToModel(sequelize_user);
+        projects.forEach((project) => {
+            user.addProject(project);
+        });
+
+        return user;
+    }
 }
 function convertSequelizeModelToModel(sequelize_user) {
     return new UserBuilder(sequelize_user.id, sequelize_user.name, sequelize_user.contact)
         .setPw(sequelize_user.pw)
+        .build();
+}
+function convertSequelizeProjectToModel(sequelize_project) {
+    return new ProjectBuilder(sequelize_project.name)
+        .setId(sequelize_project.id)
         .build();
 }
 UserRepo.init({
