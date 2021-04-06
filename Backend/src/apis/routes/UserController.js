4aff00ea99636be0da0d17ffcbe58960f213a85b
Jungwoo-Son <jungwoo9356@gmail.com>
2021-04-06 23:23:26 +0900

    [ADD] 유저의 몯느 프로젝트 가져오기 구현

--- Backend/src/apis/routes/UserController.js
+++ Backend/src/apis/routes/UserController.js
@@ -7,5 +7,11 @@ controllers.getUser = async (req, res, next) => {
     const user = await UserService.getUserById(user_id);
     return user;
 };
+controllers.getProjectsOfUser = async (req, res) => {
+    const user_id = req.params.id;
+    const projects = await UserService.getProjectsOfUser(user_id);
+
+    return projects;
+};
 
 module.exports = controllers;
