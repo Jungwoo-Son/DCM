e497171b2648fe96e1c48ab2e1ee8564d5c36159
Jungwoo-Son <jungwoo9356@gmail.com>
2021-04-06 19:04:16 +0900

    [ADD] findByIdWithProjects 테스트

--- Backend/src/specs/services/UserService.spec.js
+++ Backend/src/specs/services/UserService.spec.js
@@ -1,15 +1,20 @@
+const { ProjectBuilder } = require('../../models/Project');
 const { UserBuilder } = require('../../models/User');
-const { UserRepo } = require('../../repositories');
+const { UserRepo, ProjectRepo } = require('../../repositories');
+const UserProjectRepo = require('../../repositories/definitions/UserProjectRepo');
 const UserService = require('../../services/UserService');
 
 jest.mock('../../repositories/definitions/UserRepo');
 jest.mock('../../repositories/definitions/ProjectRepo');
 jest.mock('../../repositories/definitions/ComponentRepo');
+jest.mock('../../repositories/definitions/UserProjectRepo');
 
 
 describe('spec of UserService', () => {
     beforeEach(() => {
         UserRepo.mockClear();  
+        UserProjectRepo.mockClear();
+        ProjectRepo.mockClear();
     });
     it('should create a new user to db', async () => {
         await UserService.createNewUser('asdf', 'jklo', '김철수', 'email@gmail.com');
@@ -20,5 +25,9 @@ describe('spec of UserService', () => {
     it('should get user by id', async () => {
         const user = await UserService.getUserById('asdf');
         expect(user).toEqual(new UserBuilder('asdf', '홍길동', '010-1234-5678').setPw('qwer').build());
+    });
+    it('should list all projects of users', async () => {
+        const projects = await UserService.getProjectsOfUser('asdf');
+        expect(projects).toEqual([new ProjectBuilder('프로젝트1').setId(10).build()]);
     });
 });
