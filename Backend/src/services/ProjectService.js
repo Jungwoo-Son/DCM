d46b935f777034542ddf7d10a70039060f060f84
Jungwoo-Son <jungwoo9356@gmail.com>
2021-04-07 14:50:22 +0900

    [ADD] getMembersOfProject 구현

--- Backend/src/services/ProjectService.js
+++ Backend/src/services/ProjectService.js
@@ -12,6 +12,10 @@ class ProjectService {
         const project = await ProjectRepo.findById(id);
         return project;
     }
+    static async getMembersOfProject(id) {
+        const project = await ProjectRepo.findByIdWithMember(id);
+        return project.getMembers();
+    }
 }
 
 module.exports = ProjectService;
