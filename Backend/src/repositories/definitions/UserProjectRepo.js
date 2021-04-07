220a25b3d58f7f62bbcb31de42905ca2e6ec3345
Jungwoo-Son <jungwoo9356@gmail.com>
2021-04-07 16:12:03 +0900

    [ADD] UserProjectRepo 구현

--- Backend/src/repositories/definitions/UserProjectRepo.js
+++ Backend/src/repositories/definitions/UserProjectRepo.js
@@ -2,6 +2,25 @@ const Repo = require('./SequelizeRepo/UserProject');
 
 class UserProjectRepo {
     static repo = Repo;
+    static async findAll(transaction) {
+        const sequelize_user_projects = await this.repo.findAll({transaction});
+
+        const user_projects = sequelize_user_projects.map((sequelize_model) => convertSequelizeModelToModel(sequelize_model));
+        return user_projects;
+    }
+    static async create(user_project, transaction) {
+        await this.repo.create({
+            user_id: user_project.user_id,
+            project_id: user_project.project_id
+        },{ transaction });
+    }
+}
+
+function convertSequelizeModelToModel(sequelize_user_project) {
+    return {
+        user_id: sequelize_user_project.user_id,
+        project_id: sequelize_user_project.project_id
+    };
 }
 
 
