4ef377c77af7f46579ab848da1df9f346e2e545a
Jungwoo-Son <jungwoo9356@gmail.com>
2021-04-07 14:49:08 +0900

    [ADD] getMemberOfProject 테스트 작성

--- Backend/src/specs/services/ProjectService.spec.js
+++ Backend/src/specs/services/ProjectService.spec.js
@@ -1,15 +1,19 @@
 jest.mock('../../repositories/definitions/UserRepo');
 jest.mock('../../repositories/definitions/ProjectRepo');
 jest.mock('../../repositories/definitions/ComponentRepo');
+jest.mock('../../repositories/definitions/UserProjectRepo');
 
 const { ProjectBuilder } = require('../../models/Project');
-const { ProjectRepo } = require('../../repositories');
+const { UserBuilder } = require('../../models/User');
+const { ProjectRepo, UserRepo, UserProjectRepo } = require('../../repositories');
 const ProjectService = require('../../services/ProjectService');
 
 
 describe('spec of UserService', () => {
     beforeEach(() => {
-        ProjectRepo.mockClear();  
+        ProjectRepo.mockClear(); 
+        UserRepo.mockClear();
+        UserProjectRepo.mockClear();
     });
     it('should create a new project to db', async () => {
         const new_project = new ProjectBuilder('프로젝트2').build();
@@ -20,5 +24,10 @@ describe('spec of UserService', () => {
     it('should get project by id', async () => {
         const project = await ProjectService.getProjectById(10);
         expect(project).toEqual(new ProjectBuilder('프로젝트1').setId(10).build());
+    });
+    it('should get all member of the project', async () => {
+        const members = await ProjectService.getMembersOfProject(10);
+        const expected_member = new UserBuilder('asdf', '홍길동', '010-1234-5678').setPw('qwer').build();
+        expect(members).toEqual([expected_member]);
     });
 });
