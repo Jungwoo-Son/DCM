de5d300c23980ce06da37d673a8acae7eb617fd8
Jungwoo-Son <jungwoo9356@gmail.com>
2021-04-16 09:47:22 +0900

    [ADD] getDependenciesOfTheComponent 구현

--- Backend/src/services/ComponentService.js
+++ Backend/src/services/ComponentService.js
@@ -12,6 +12,10 @@ class ProjectService {
         const components = await ComponentRepo.findByProjectId(project_id);
         return components;
     }
+    static async getDependenciesOfTheComponent(project_id, component_id) {
+        const components = await ComponentRepo.findByIdWithDependencies(component_id);
+        return components.getDependencies();
+    }
 }
 
 module.exports = ProjectService;
