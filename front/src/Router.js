5a0c486e06d287b72b03544e984eb58d18c46aff
Jungwoo-Son <jungwoo9356@gmail.com>
2021-05-12 22:11:32 +0900

    feat: Route ReigisterPage

--- front/src/Router.js
+++ front/src/Router.js
@@ -1,9 +1,12 @@
 import { Route, Switch } from "react-router-dom";
 import MainPage from "./app/page/main/MainPage";
+import RegisterPage from "./app/page/register/RegisterPage";
+
 const AppRouter = () => {
   return (
     <Switch>
-      <Route path="/" component={MainPage} />
+      <Route exact path="/" component={MainPage} />
+      <Route exact path="/signup" component={RegisterPage} />
     </Switch>
   );
 };
