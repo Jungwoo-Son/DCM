9727a930f995879614ec6e751d05f1ed38544029
Jungwoo-Son <jungwoo9356@gmail.com>
2021-05-12 22:11:44 +0900

    feat: Init RegisterPage

---
+++ front/src/app/page/register/RegisterPage.js
@@ -0,0 +1,11 @@
+import HeaderContainer from "../../container/header/HeaderCotainer";
+
+const RegisterPage = () => {
+  return (
+    <>
+      <HeaderContainer />
+    </>
+  );
+};
+
+export default RegisterPage;
