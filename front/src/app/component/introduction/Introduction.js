e238acb7c80b0774a6d3749a3caf9e64a418c3fa
Jungwoo-Son <jungwoo9356@gmail.com>
2021-05-12 19:48:31 +0900

    feat: Lay out Introduction

--- front/src/app/component/introduction/Introduction.js
+++ front/src/app/component/introduction/Introduction.js
@@ -1,6 +1,20 @@
 import * as S from "./style";
+import Image from "../../../assets/DescriptionImage.png";
+
 const Introduction = () => {
-  return <S.Introduction></S.Introduction>;
+  return (
+    <S.Introduction>
+      <S.DescriptionImg src={Image} />
+      <S.BigDescription>프로젝트 의존성 관리툴</S.BigDescription>
+      <S.SmallDescription>
+        복잡한 프로젝트에서 효율적인 의사소통을 위한 최고의 선택
+        <br />
+        프로젝트에서 복잡한 구성요소간 관계와 담당자를 빠르게 관리할 수
+        있습니다.
+      </S.SmallDescription>
+      <S.SignUpBtn>회원가입</S.SignUpBtn>
+    </S.Introduction>
+  );
 };
 
 export default Introduction;
--- front/src/app/component/introduction/style.js
+++ front/src/app/component/introduction/style.js
@@ -1,6 +1,44 @@
 import styled from "styled-components";
 
 export const Introduction = styled.div`
-  height: 50px;
-  background-color: #000000;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  background: linear-gradient(to bottom right, #00ffcb, #03a6dc);
+`;
+export const DescriptionImg = styled.img`
+  margin-top: 70px;
+
+  width: 30rem;
+  height: 12.06rem;
+`;
+export const BigDescription = styled.span`
+  text-align: center;
+
+  margin-top: 20px;
+
+  font-size: 4.625rem;
+  color: #ffffff;
+  font-weight: bold;
+`;
+export const SmallDescription = styled.span`
+  text-align: center;
+
+  margin-top: 1.4375rem;
+
+  font-size: 1.25rem;
+  color: #666666;
+  font-weight: bold;
+`;
+export const SignUpBtn = styled.button`
+  margin: 4.5rem 0 6.563rem 0;
+
+  width: 10.88rem;
+  height: 2.25rem;
+
+  border: none;
+  border-radius: 1.125rem;
+  background-color: #ffffff;
+  box-shadow: 0 6px 6px rgba(0, 0, 0, 0.16);
 `;
