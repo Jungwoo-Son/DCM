c8ee346772ad26fd05cb2cb5870a139d1846be1b
Jungwoo-Son <jungwoo9356@gmail.com>
2021-05-13 19:31:04 +0900

    fix: Use custom router

--- Backend/src/apis/routes/index.js
+++ Backend/src/apis/routes/index.js
@@ -1,11 +1,10 @@
-const { Router } = require("express");
+const Router = require("./CustomRouter");
 const AuthenticationControllers = require("./AuthenticationController");
 
-const router = Router();
-const router_passed_middleware = Router();
+const router = new Router();
 
 router.post("/register", AuthenticationControllers.register);
 
 module.exports = {
-  router,
+  router: router.express_router,
 };
