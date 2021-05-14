894e916b9c056d589d56a744d06ad2fcb7d6d21f
Jungwoo-Son <jungwoo9356@gmail.com>
2021-05-14 11:29:04 +0900

    feat: Implement AccessTokenExtractor

---
+++ Backend/src/apis/middlewares/index.js
@@ -0,0 +1,12 @@
+const { AccessToken } = require("../../authentication");
+
+async function AccessTokenExtractor(req, res) {
+  const stringfied_token = req.get("Authorization");
+
+  const access_token = await AccessToken.fromString(stringfied_token);
+  req.auth = access_token;
+}
+
+module.exports = {
+  AccessTokenExtractor,
+};
