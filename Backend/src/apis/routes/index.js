a4bbbcdeca1044198559e3468999337f667cd11b
Jungwoo-Son <jungwoo9356@gmail.com>
2021-05-14 10:03:50 +0900

    feat: Route login

--- Backend/src/apis/routes/index.js
+++ Backend/src/apis/routes/index.js
@@ -4,6 +4,7 @@ const AuthenticationControllers = require("./AuthenticationController");
 const router = new Router();
 
 router.post("/register", AuthenticationControllers.register);
+router.post("/login", AuthenticationControllers.login);
 
 module.exports = {
   router: router.express_router,
