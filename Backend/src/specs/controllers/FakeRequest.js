eb7c7f06e1942f9040511c5d85ff7d27e92a4241
Jungwoo-Son <jungwoo9356@gmail.com>
2021-05-14 11:28:27 +0900

    test: Apply header

--- Backend/src/specs/controllers/FakeRequest.js
+++ Backend/src/specs/controllers/FakeRequest.js
@@ -1,26 +1,34 @@
 class FakeRequest {
-    constructor(data) {
-        this.body = data.body;
-        this.params = data.params;
-        this.auth = data.auth;
-    }
+  constructor(data) {
+    this.body = data.body;
+    this.params = data.params;
+    this.auth = data.auth;
+    this.header = data.header;
+  }
+  get(attribute) {
+    return this.header[attribute];
+  }
 }
 class FakeRequestBuilder {
-    setParams(params) {
-        this.params = params;
-        return this;
-    }
-    setBody(body) {
-        this.body = body;
-        return this;
-    }
-    setAuth(auth) {
-        this.auth = auth;
-        return this;
-    }
-    build() {
-        return new FakeRequest(this);
-    }
+  setParams(params) {
+    this.params = params;
+    return this;
+  }
+  setBody(body) {
+    this.body = body;
+    return this;
+  }
+  setAuth(auth) {
+    this.auth = auth;
+    return this;
+  }
+  setHeader(header) {
+    this.header = header;
+    return this;
+  }
+  build() {
+    return new FakeRequest(this);
+  }
 }
 
 module.exports = FakeRequestBuilder;
